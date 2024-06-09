6602871cc10991bcaf6bedf0eedb7015a49d217b
om <ommotwani06@gmail.com>
2024-06-10 00:18:13 +0530

    implement serch functionality in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { DataService } from './services/data.service';
+import { product } from './models/product';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'search-functionality-angular';
+  products: product[] = [];
+  searchForm: FormGroup;
+  loading: boolean = true;
+
+  constructor(
+    private productService: DataService,
+    private fb: FormBuilder
+  ) {
+    this.searchForm = this.fb.group({
+      searchTerm: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.productService.getData().subscribe((data: product[]) => {
+      this.products = data;
+    });
+
+    this.searchForm.get('searchTerm')?.valueChanges.subscribe(searchTerm => {
+      this.filterProducts(searchTerm);
+    });
+  }
+
+  filterProducts(searchTerm: string): void {
+    if (searchTerm) {
+      this.products = this.products.filter(product =>
+        product.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+    } else {
+      // Re-fetch products if the search term is cleared
+      this.productService.getData().subscribe((data: product[]) => {
+        this.products = data;
+      });
+    }
+  }
+
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
+import {MatTableModule} from '@angular/material/table'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,10 +14,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatTableModule,
+    ReactiveFormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
